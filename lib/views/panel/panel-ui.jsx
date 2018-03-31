4a034f2470c29ba4ef49031dc40a54a84f064e63
Jacob84 <jacob.mendoza@gmail.com>
2018-03-31 16:12:54 +0200

    Initial implementation of the Atom's Docks API

--- lib/views/panel/panel-ui.jsx
+++ lib/views/panel/panel-ui.jsx
@@ -5,11 +5,44 @@ import ReactDOM from 'react-dom';
 import MainContainer from './main-container.jsx';
 
 class PanelUI {
-	constructor(element) {
-		this.mainContainer = ReactDOM.render(<MainContainer/>, element);
+	constructor() {
+		this.element = document.createElement('div');
+		this.mainContainer = ReactDOM.render(<MainContainer/>, this.element);
+	}
+
+	getTitle() {
+		return 'rspec-tree-runner';
+	}
+
+	getURI () {
+		return 'atom://rspec-tree-runner/panel';
+	}
+
+	getDefaultLocation() {
+		return 'right';
+	}
+
+	getAllowedLocations() {
+		return ['left', 'right'];
+	}
+
+	getPreferredWidth() {
+		return 300;
+	}
+
+	resizeSubContainer() {
+		// Quick fix implemented to allow the subContainer to have scroll when
+		// the list of tests is big. I haven't been able to fix it just with CSS.
+		const newHeight = window.innerHeight - 60;
+		const subContainer = document.getElementById('subContainer');
+
+		if (subContainer !== null) {
+			subContainer.style.height = `${newHeight}px`;
+		}
 	}
 
 	updateState(newState) {
+		this.resizeSubContainer();
 		this.mainContainer.setState(newState);
 	}
 }
